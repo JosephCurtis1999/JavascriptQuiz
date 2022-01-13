9b7a9e38156bb9785f70ca49b547ee5d644d54c6
Joe Curtis <joecurtis9999@gmail.com>
2022-01-13 15:14:06 +0000

    Wrote out a questionbank in javascript

---
+++ Assets/js/index.js
@@ -0,0 +1,27 @@
+var questions = [
+    {
+        title: "How do you link a javascript file to your html file?",
+        choices: ["link tag", "script src tag", "save to folder"],
+        answer: "script src tag"
+    },
+    {
+        title: "Which one of the following is a javascript data type?",
+        choices: ["string", "function", "button"],
+        answer: "string"
+    },
+    {
+        title: "What operator is used for comparing both values and types?",
+        choices: ["===", "==", ">="],
+        answer: "==="
+    },
+    {
+        title: "Is Javascript a statically typed or dynamically typed language?",
+        choices: ["statically", "dynamically"],
+        answer: "dynamically"
+    },
+    {
+        title: "Which one of the following is a way to declare a javascript variable?",
+        choices: ["choose", "select", "const"],
+        answer: "const"
+    }
+]
